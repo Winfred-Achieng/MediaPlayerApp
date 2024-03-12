26b1055c84071cf55a8cf1f05de7b88b79e9c08e
winfred <achiengwinfred20@gmail.com>
2024-03-12 11:32:13 +0300

    custom service

--- app/src/main/java/com/winfred/musicplayerapp/MainActivity.java
+++ app/src/main/java/com/winfred/musicplayerapp/MainActivity.java
@@ -2,13 +2,39 @@ package com.winfred.musicplayerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startBtn, stopBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn= findViewById(R.id.start);
+        stopBtn= findViewById(R.id.stop);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent serviceIntent=new Intent(getApplicationContext(),CustomService.class);
+                startService(serviceIntent);
+            }
+        });
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent serviceIntent=new Intent(getApplicationContext(),CustomService.class);
+                stopService(serviceIntent);
+            }
+        });
+
     }
 }
